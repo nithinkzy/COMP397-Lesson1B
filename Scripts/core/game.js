23f7e679179e5e08871fa6c68213cc8e41f276f2
Nithin Kumar Kollerethu Suresh <nkollere@my.centennialcollege.ca>
2018-06-15 15:24:35 -0400

    npm support 2

---
+++ Scripts/core/game.js
@@ -0,0 +1,25 @@
+//IIFE = IMMEDIATELY INVOKED FUCTION EXPRESSION
+(function(){
+
+function Init(){
+
+    console.log("Initialization Started")
+    
+    let myVariable = "foo";
+    let myVariable = 1;
+}
+
+function Start(){
+
+}
+
+function Update(){
+
+}
+
+function Main(){
+
+}
+
+window.onload = Init;
+})();
