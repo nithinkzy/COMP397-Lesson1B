a6a43b022cf030cb9cd0b22be73b8ea5f523b30c
Nithin Kumar Kollerethu Suresh <nkollere@my.centennialcollege.ca>
2018-07-23 18:15:38 -0400

    Added typescript and createjs support

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,25 +1,31 @@
 //IIFE = IMMEDIATELY INVOKED FUCTION EXPRESSION
-(function(){
-
-function Init(){
-
-    console.log("Initialization Started")
-    
-    let myVariable = "foo";
-    let myVariable = 1;
-}
-
-function Start(){
-
-}
-
-function Update(){
-
-}
-
-function Main(){
-
-}
-
-window.onload = Init;
+(function () {
+    // Game Variables
+    var canvas = document.getElementById("canvas");
+    var stage;
+    var helloLabel;
+    function Init() {
+        console.log("Initialization Started");
+        Start();
+    }
+    function Start() {
+        console.log("Starting Application..");
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; //60 fps
+        createjs.Ticker.on("tick", Update);
+        Main();
+    }
+    function Update() {
+        helloLabel.rotation += 5;
+        stage.update(); // redraws the stage
+    }
+    function Main() {
+        console.log("Game started..");
+        helloLabel = new createjs.Text("Hello! World,", "40px Consolas", "#000000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+        stage.addChild(helloLabel);
+    }
+    window.onload = Init;
 })();
+//# sourceMappingURL=game.js.map
